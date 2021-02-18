28119a53693b336c520503889a97612563c0967b
Mary.Qian <maryqian@augmentum.com.cn>
2021-02-18 09:53:58 +0800

    0218

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div>------</div>
+    <!-- <HelloWorld msg="second"/> -->
+
   </div>
 </template>
 
